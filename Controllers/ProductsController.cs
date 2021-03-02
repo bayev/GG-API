ce848ce020187eed323e55b83d0ec379d42d28f0
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-03-02 15:11:34 +0100

    Added authorization to productscontroller
    Added code so that you have to have either "admin" or "root" in the userID to be authorized to use the productcontroller. This is implemented through try-catch in the respective method in the controller.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -28,52 +28,109 @@ namespace Api.Controllers
         [HttpGet("all")]
         public async Task<ActionResult> GetAllProducts()
         {
-            
-            List<Product> listOfProducts = new List<Product>();
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+            var roles = await _userManager.GetRolesAsync(user);
 
-            if (!listOfProducts.Any())
+            if (roles.Contains("root") || roles.Contains("admin"))
             {
-                var query = _context.Products;
+                List<Product> listOfProducts = new List<Product>();
 
-                foreach (var item in query)
+                if (!listOfProducts.Any())
                 {
-                    Product product = new Product();
-                    product.Id = item.Id;
-                    product.Name = item.Name;
-                    product.Price = item.Price;
-                    product.Weight = item.Weight;
-                    product.Description = item.Description;
-                    product.Image = item.Image;
-                    product.Category = item.Category;
-                    product.CreateDate = item.CreateDate;
-                    product.Stock = item.Stock;
-                    product.Size = item.Size;
-                    product.Brand = item.Brand;
+                    var query = _context.Products;
 
-                    listOfProducts.Add(product);
+                    foreach (var item in query)
+                    {
+                        Product product = new Product();
+                        product.Id = item.Id;
+                        product.Name = item.Name;
+                        product.Price = item.Price;
+                        product.Weight = item.Weight;
+                        product.Description = item.Description;
+                        product.Image = item.Image;
+                        product.Category = item.Category;
+                        product.CreateDate = item.CreateDate;
+                        product.Stock = item.Stock;
+                        product.Size = item.Size;
+                        product.Brand = item.Brand;
+
+                        listOfProducts.Add(product);
+                    }
+                    return Ok(listOfProducts);
+
                 }
-                return Ok(listOfProducts);
-
+                else
+                    return NotFound(new { message = "Sorry, no products found" });
             }
             else
-            {
-                return NotFound(new { message = "Sorry, no products found" });
-            }
-
-
-            
+                return Unauthorized();
         }
 
         [HttpPut("update/{productid}")]
         public async Task<ActionResult> UpdateProduct([FromBody] PostProductModel model, string Productid)
         {
-            try
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles.Contains("root") || roles.Contains("admin"))
             {
-                Product product = _context.Products.Where(x => x.Id == Productid).FirstOrDefault();
+                try
+                {
+                    Product product = _context.Products.Where(x => x.Id == Productid).FirstOrDefault();
+                    product.Name = model.Name;
+                    product.Price = Convert.ToDecimal(model.Price);
+                    product.Weight = model.Weight;
+                    product.Description = model.Description;
+                    // upload image the proper way
+                    product.Image = model.Image;
+                    product.Category = model.Category;
+                    product.Stock = model.Stock;
+                    product.Size = model.Size;
+                    product.Brand = model.Brand;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = $"Sorry, something happend. {ex.ToString()}" });
+                }
+            }
+            else
+                return Unauthorized();
+        }
+
+        [HttpGet("get/{productid}")]
+        public async Task<ActionResult> GetProduct([FromRoute] string productid)
+        {
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles.Contains("root") || roles.Contains("admin"))
+            {
+                Product product = _context.Products.Where(x => x.Id == productid).FirstOrDefault();
+
+                return Ok(product);
+            }
+            else
+                return Unauthorized();
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateProduct([FromBody] PostProductModel model)
+        {
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles.Contains("root") || roles.Contains("admin"))
+            {
+                Product product = new Product();
+
                 product.Name = model.Name;
                 product.Price = Convert.ToDecimal(model.Price);
                 product.Weight = model.Weight;
                 product.Description = model.Description;
+
                 // upload image the proper way
                 product.Image = model.Image;
                 product.Category = model.Category;
@@ -81,93 +138,50 @@ namespace Api.Controllers
                 product.Size = model.Size;
                 product.Brand = model.Brand;
 
-                await _context.SaveChangesAsync();
+                try
+                {
+                    _context.Products.Add(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = $"Sorry, something happend. {ex.ToString()}" });
+                }
+
+                return Ok();
+
             }
-            catch (Exception ex)
+            else
             {
-                return BadRequest(new { message = $"Sorry, something happend. {ex.ToString()}" });
+                return Unauthorized();
             }
-            return Ok();
-        }
-
-        [HttpGet("get/{productid}")]
-        public async Task<ActionResult> GetProduct([FromRoute] string productid)
-        {
-            Product product = _context.Products.Where(x => x.Id == productid).FirstOrDefault();
-
-            return Ok(product);
-        }
-
-        [HttpPost("create")]
-        public async Task<ActionResult> CreateProduct([FromBody] PostProductModel model)
-        {
-
-            Product product = new Product();
-
-            product.Name = model.Name;
-            product.Price = Convert.ToDecimal(model.Price);  
-            product.Weight = model.Weight;
-            product.Description = model.Description;
-
-            // upload image the proper way
-            product.Image = model.Image;
-            product.Category = model.Category;
-            product.Stock = model.Stock;
-            product.Size = model.Size;
-            product.Brand = model.Brand;
-
-            try
-            {
-                _context.Products.Add(product);
-                await _context.SaveChangesAsync();
-            }
-            catch(Exception ex)
-            {
-                return BadRequest(new { message = $"Sorry, something happend. {ex.ToString()}" });
-            }
-
-            return Ok();
-
-            //User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
-
-            //var roles = await _userManager.GetRolesAsync(user);
-
-            //roles.Contains("admin");
-
-            //if (user != null)
-            //{
-            //    Product newProduct = new Product();
-
-            //    //newProduct.Price = 99.5
-
-            //    _context.Products.Add(newProduct);
-
-            //    _context.SaveChanges();
-
-            //    return Ok(new { message = $"Success! Product with ID {newProduct.Id} was created." });
-
-            //}
-            //else
-            //{
-            //    return Unauthorized(new { message = "user was not found, sorry!" });
-            //}
         }
         [HttpDelete("delete/{productid}")]
         public async Task<ActionResult> DeleteProduct([FromRoute] string productid)
         {
-            try
-            {
-                Product product = _context.Products.Where(x => x.Id == productid).FirstOrDefault();
-                _context.Remove(product);
-                await _context.SaveChangesAsync();
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+            var roles = await _userManager.GetRolesAsync(user);
 
-            }
-            catch (Exception ex)
+            if (roles.Contains("root") || roles.Contains("admin"))
             {
-                return BadRequest(new { message = $"Sorry, something happend. {ex.ToString()}" });
+                try
+                {
+                    Product product = _context.Products.Where(x => x.Id == productid).FirstOrDefault();
+                    _context.Remove(product);
+                    await _context.SaveChangesAsync();
 
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = $"Sorry, something happened. {ex.ToString()}"});
+
+                }
+                return Ok();
             }
-            return Ok();
+            else
+            {
+                return Unauthorized();
+            }
         }
 
 
