cc5f7eaa806baf539761d4612055022db3d407eb
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-03-25 21:38:01 +0100

    Added Methods for GetAllc2a and deletec2a
    A new model is added in order to send relevant information to frondend using the hhtpresponsemessages

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Text.Json;
+using Api.Models;
 
 namespace Api.Controllers
 {
@@ -22,49 +24,58 @@ namespace Api.Controllers
             _userManager = userManager;
         }
 
-        [HttpGet("all")]
-        public async Task<ActionResult> GetAllProducts()
+        [HttpGet("GetCart")]
+        public async Task<ActionResult> GetAllProductsInCart()
         {
             User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
-            var roles = await _userManager.GetRolesAsync(user);
 
-            if (roles.Contains("root") || roles.Contains("admin"))
+            Cart cart = _context.Carts
+                .Where(x => x.UserId == user.Id)
+                .FirstOrDefault();
+
+            
+            if(user != null)
             {
-                List<Product> listOfProducts = new List<Product>();
+                var allC2P = _context.CartToProducts
+                .Where(x => x.Cart == cart).ToList();
 
-                if (!listOfProducts.Any())
+                var C2PList = new List<c2aReturnModel>();
+
+                foreach (var c2p in allC2P)
                 {
-                    var query = _context.Products;
-
-                    foreach (var item in query)
+                    var product = _context.Products.Where(x => x.Id == c2p.ProductId).FirstOrDefault();
+                    c2aReturnModel c2aRM = new c2aReturnModel
                     {
-                        Product product = new Product();
-                        product.Id = item.Id;
-                        product.Name = item.Name;
-                        product.Price = item.Price;
-                        product.Weight = item.Weight;
-                        product.Description = item.Description;
-                        product.Image = item.Image;
-                        product.Category = item.Category;
-                        product.CreateDate = item.CreateDate;
-                        product.Stock = item.Stock;
-                        product.Size = item.Size;
-                        product.Brand = item.Brand;
-
-                        listOfProducts.Add(product);
-                    }
-                    return Ok(listOfProducts);
-
+                        Id = product.Id,
+                        Name = product.Name,
+                        Price = product.Price,
+                        Weight = product.Weight,
+                        Description = product.Description,
+                        Image = product.Image,
+                        Category = product.Category,
+                        CreateDate = product.CreateDate,
+                        Size = product.Size,
+                        Brand = product.Brand,
+                        Amount = c2p.Amount,
+                        c2pID = c2p.Id
+                    };
+                    C2PList.Add(c2aRM);
                 }
+
+
+                if (C2PList != null)
+                    return Ok(C2PList);
                 else
-                    return NotFound(new { message = "Sorry, no products found" });
+                    return Ok("cart empty");
             }
             else
-                return Unauthorized();
+            {
+                return NotFound();
+            }
         }
 
         [HttpPost("addtocart")]
-        public async Task<ActionResult> CreateProduct([FromBody] string productId)
+        public async Task<ActionResult> CreateCartToProduct([FromBody] string productId)
         {
             User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
 
@@ -92,18 +103,45 @@ namespace Api.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            CartToProduct c2a = new CartToProduct();
+            CartToProduct c2p = new CartToProduct();
             productInDb.Stock -= 1;
-            c2a.Amount += 1;
-            c2a.ProductId = productInDb.Id;
-            c2a.Cart = cart;
-            _context.Add(c2a);
+            c2p.Amount += 1;
+            c2p.ProductId = productInDb.Id;
+            c2p.Cart = cart;
+            _context.Add(c2p);
             await _context.SaveChangesAsync();
 
             var allC2P = _context.CartToProducts
                 .Where(x => x.Cart == cart).ToList();
 
             return Ok();
+        }
+        [HttpDelete("deletefromcart")]
+        public async Task<ActionResult> DeleteFromCart([FromBody] string c2pId)
+        {
+            User user = await _userManager.FindByNameAsync(User.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name)).Value);
+
+            if(user != null)
+            {
+                CartToProduct c2p = _context.CartToProducts.Where(x => x.Id == c2pId).FirstOrDefault();
+                Product product = _context.Products.Where(x => x.Id == c2p.ProductId).FirstOrDefault();
+                if(c2p != null && product != null)
+                {
+                    product.Stock += 1;
+
+                    _context.Remove(c2p);
+                    await _context.SaveChangesAsync();
+                    return Ok("Deleted");
+                }
+                else
+                {
+                    return BadRequest("c2p or product not found");
+                }
+            }
+            else
+            {
+                return NotFound("User not found");
+            }
         }
     }
 }
